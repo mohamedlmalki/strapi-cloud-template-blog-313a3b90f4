e69d0e43440381d22d2b5dbb6003438b56ced842
mohamedlmalki <mohamedlmalki@yahoo.com>
2025-03-05 02:46:41 +0000

    first commit

---
+++ src/api/bb/controllers/bb.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * bb controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::bb.bb');
---
+++ src/api/bb/routes/bb.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * bb router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::bb.bb');
