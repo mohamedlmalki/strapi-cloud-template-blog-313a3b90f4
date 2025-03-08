d465800a69b7a0b53c267a74f396b6e972295fbd
mohamedlmalki <mohamedlmalki@yahoo.com>
2025-03-08 02:02:36 +0000

    Delete src directory

--- src/api/lecontact/controllers/lecontact.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * lecontact controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::lecontact.lecontact');
--- src/api/lecontact/routes/lecontact.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * lecontact router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::lecontact.lecontact');
