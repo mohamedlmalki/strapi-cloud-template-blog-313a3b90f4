b771dd0210eb30a79dec3294dd44c6297616f412
mohamedlmalki <mohamedlmalki@yahoo.com>
2025-03-08 02:58:42 +0000

    Added new files

---
+++ src/api/lecontact/controllers/lecontact.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * lecontact controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::lecontact.lecontact');
---
+++ src/api/lecontact/routes/lecontact.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * lecontact router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::lecontact.lecontact');
