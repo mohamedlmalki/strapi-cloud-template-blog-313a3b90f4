2a304c26da0eda291c455b43330ffcf8c5b75ee6
mohamedlmalki <mohamedlmalki@yahoo.com>
2025-05-07 02:06:09 +0200

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,12 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  // ...
+  email: {
+    config: {
+      provider: 'sendmail',
+      settings: {
+        defaultFrom: '"Your Sender Name" <no-reply@strapi.io>',
+      },
+    },
+  },
+  // ...
+});
